2f38d8bd5b3f3f3b6764c14ad7ec179692b55b37
Michele Cantelli <emmekappa@gmail.com>
2018-06-26 01:11:28 +0200

    removing

--- modules/docker/codebuild/main.tf
+++ modules/docker/codebuild/main.tf
@@ -198,54 +198,6 @@ resource "aws_codebuild_project" "default" {
   tags = "${module.label.tags}"
 }
 
-resource "aws_codebuild_project" "terraform" {
-  name         = "${module.label.id}-terraform"
-  service_role = "${aws_iam_role.default.arn}"
-
-  artifacts {
-    type = "CODEPIPELINE"
-
-    #location = "${aws_s3_bucket.cache_bucket.bucket}"
-  }
-
-  # The cache as a list with a map object inside.
-  cache = ["${local.cache}"]
-
-  environment {
-    compute_type    = "${var.build_compute_type}"
-    image           = "${var.build_image}"
-    type            = "LINUX_CONTAINER"
-    privileged_mode = "${var.privileged_mode}"
-
-    environment_variable = [
-      {
-        "name"  = "AWS_REGION"
-        "value" = "${signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name}"
-      },
-      {
-        "name"  = "AWS_ACCOUNT_ID"
-        "value" = "${signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id}"
-      },
-      {
-        "name"  = "SLACK_WEBHOOK_URL"
-        "value" = "${signum(length(var.slack_webhook_url)) == 1 ? var.slack_webhook_url : "UNSET"}"
-      },
-      {
-        "name"  = "GITHUB_TOKEN"
-        "value" = "${signum(length(var.github_token)) == 1 ? var.github_token : "UNSET"}"
-      },
-      "${var.environment_variables}",
-    ]
-  }
-
-  source {
-    type      = "CODEPIPELINE"
-    buildspec = "buildspec_terraform_plan.yml"
-  }
-
-  tags = "${module.label.tags}"
-}
-
 resource "aws_codebuild_webhook" "default" {
   project_name = "${aws_codebuild_project.default.name}"
   depends_on   = ["aws_codebuild_project.default"]
