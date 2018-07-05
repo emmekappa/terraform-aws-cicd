9e93d8601bd5b9177d1d78841c0c7661e3f31e50
Michele Cantelli <emmekappa@gmail.com>
2018-07-05 19:47:38 +0200

    Adding more permissions

--- modules/terraform/codebuild/main.tf
+++ modules/terraform/codebuild/main.tf
@@ -54,6 +54,8 @@ data "aws_iam_policy_document" "permissions" {
       "sns:*",
       "sqs:*",
       "application-autoscaling:*",
+      "cloudwatch:*",
+      "events:*",
     ]
 
     effect = "Allow"
