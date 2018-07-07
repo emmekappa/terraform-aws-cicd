35c8a3bc055612eec4715d7e07bfc8ca610cefe9
Michele Cantelli <emmekappa@gmail.com>
2018-07-07 17:09:26 +0200

    Adding dependency

--- modules/codepipeline-chatops/main.tf
+++ modules/codepipeline-chatops/main.tf
@@ -117,6 +117,8 @@ resource "aws_lambda_function" "handle_approval" {
       SLACK_VERIFICATION_TOKEN = "${var.slack_verification_token}"
     }
   }
+
+  depends_on = ["${aws_iam_role.lambda}"]
 }
 
 resource "aws_api_gateway_rest_api" "default" {
